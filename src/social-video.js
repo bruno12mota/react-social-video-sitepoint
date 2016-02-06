f740e1c6051d11d8fdb2aa421a3acf8edb891a1b
Bruno Mota <bruno12mota@gmail.com>
2016-02-06 17:55:35 +0100

    DRY videos url parse logic as to @craigbilner suggestion

--- src/social-video.js
+++ src/social-video.js
@@ -1,3 +1,4 @@
+import qs from 'query-string';
 import React, {Component, PropTypes} from 'react';
 
 export default class SocialVideo extends Component {
@@ -6,56 +7,23 @@ export default class SocialVideo extends Component {
     video: PropTypes.string.isRequired
   };
 
-  parseYoutubeURL (url) {
-    const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    let result;
-    if (match && match[7].length === 11) {
-      result = match[7];
-    } else {
-      result = false;
-    }
-    return result;
-  }
+  static urlMap = new Map([
+    ['youtube', 'http://www.youtube.com/embed/'],
+    ['vimeo', 'http://player.vimeo.com/video/'],
+    ['dailymotion', 'http://www.dailymotion.com/embed/video/']
+  ]);
 
-  parseVimeoURL (url) {
-    const regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
-    const match = regExp.exec(url);
-    let result;
-    if (match && match[5].length === 9) {
-      result = match[5];
-    } else {
-      result = false;
-    }
-    return result;
-  }
+  getIdFromVideoString (vString) {
+    const urlArr = vString.split('/');
+    const idString = urlArr[urlArr.length - 1];
+    const queryParams = qs.extract(vString);
 
-  parseDailymotionURL (url) {
-    const regExp = /^.+dailymotion.com\/((video|hub)\/([^_]+))?[^#]*(#video=([^_&]+))?/;
-    const match = url.match(regExp);
-    let result;
-    if (match && match.length > 2) {
-      result = match[5] || match[3];
-    } else {
-      result = false;
-    }
-    return result;
+    return (queryParams && qs.parse(queryParams).v) || idString || '';
   }
 
   render() {
     const {service, video, ...htmlTags} = this.props;
-
-    let src = '';
-    if (this.props.service === 'youtube') {
-      const parsedID = this.parseYoutubeURL(this.props.video);
-      src = 'http://www.youtube.com/embed/' + (parsedID || this.props.video);
-    } else if (this.props.service === 'vimeo') {
-      const parsedID = this.parseVimeoURL(this.props.video);
-      src = 'http://player.vimeo.com/video/' + (parsedID || this.props.video);
-    } else if (this.props.service === 'dailymotion') {
-      const parsedID = this.parseDailymotionURL(this.props.video);
-      src = 'http://www.dailymotion.com/embed/video/' + (parsedID || this.props.video);
-    }
+    const src = `${SocialVideo.urlMap.get(service)}${this.getIdFromVideoString(video)}`;
 
     return (
       <iframe
